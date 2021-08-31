0e388ac457d43c538f2d67715cb2d8578a1ac4bd
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-08-31 11:21:35 +0900

    Add: main 레이아웃 구현

---
+++ src/pages/Main/ButtonProductList.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+
+class ButtonProductList extends Component {
+  render() {
+    return (
+      <>
+        <div className="productList">
+          <div className="productStatus">
+            <h3>이 상품 어때요?</h3>
+          </div>
+        </div>
+        <div className="buttonCategory">
+          <ul className="buttonListCategory">
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+            <li>
+              <a href="#">추석 선물세트</a>
+            </li>
+          </ul>
+        </div>
+        <div className="listGoods">
+          <div className="boxWrapper">
+            <div className="boxViewport">
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="./images/logo2.png" />
+                  </a>
+                  <span className="productName">
+                    <a href="#">[우리밀]부침가루 </a>
+                  </span>
+                  <span className="price">3,000원</span>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./images/logo2.png" />
+                  </a>
+                  <span className="productName">
+                    <a href="#">[우리밀]부침가루 </a>
+                  </span>
+                  <span className="price">
+                    <a href="#">3,000원 </a>
+                  </span>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./images/logo2.png" />
+                  </a>
+                  <span className="productName">
+                    <a href="#">[우리밀]부침가루 </a>
+                  </span>
+                  <span className="price">
+                    <a href="#">3,000원 </a>
+                  </span>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./images/logo2.png" />
+                  </a>
+                  <span className="productName">
+                    <a href="#">[우리밀]부침가루 </a>
+                  </span>
+                  <span className="price">
+                    <a href="#">3,000원 </a>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ButtonProductList;
---
+++ src/pages/Main/CenterAdd.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CenterAdd extends Component {
+  render() {
+    return (
+      <div>
+        <div className="centerAdd">
+          <img src="./images/ad2.webp" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CenterAdd;
