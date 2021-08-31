0681769e5971dd1b311a216114f530cb88aac26e
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-01 01:33:26 +0900

    Fix: Navbar 피드백 반영 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Footer from "./components/Footer/Footer";
-import Main from "./pages/Main/Main";
+import Nav from "./components/Nav/Nav";
 import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
 import SignUp from "./pages/SignUp/SignUp";
+import Footer from "./components/Footer/Footer";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/" component={Main} />
