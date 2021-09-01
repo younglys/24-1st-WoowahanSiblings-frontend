46ab9dc2d647941284caf227352cdece0e0cb4fc
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-01 18:03:20 +0900

    Fix: 피드백 반영하여 수정 하였습니다.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,57 +5,14 @@ import "./Nav.scss";
 export default class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      itemList: [],
-      isMouseHoverEvent: false,
-      isMouseHoverNextEvent: false,
-    };
   }
-
-  componentDidMount() {
-    fetch("http://localhost:3000/data/navmockdata.json", {
-      method: "GET",
-    })
-      .then(res => res.json())
-      .then(itemListData => {
-        this.setState({
-          itemList: itemListData,
-        });
-      });
-  }
-
-  downBoxEvent = () => {
-    if (this.state.isMouseHoverEvent === false) {
-      this.setState({
-        isMouseHoverEvent: true,
-      });
-    } else if (this.state.isMouseHoverEvent === true) {
-      this.setState({
-        isMouseHoverEvent: false,
-      });
-    }
-  };
-
-  nextBoxNext = () => {
-    if (this.state.isMouseHoverNextEvent === false) {
-      this.setState({
-        isMouseHoverNextEvent: true,
-      });
-    } else if (this.state.isMouseHoverNextEvent === true) {
-      this.setState({
-        isMouseHoverNextEvent: false,
-      });
-    }
-  };
-
   render() {
-    console.log(this.state.itemList);
     return (
       <nav className="navLayout">
         <div className="firstLayout">
           <div className="kurlyMarket">
             <a href="#">
-              <img src="/images/deli.webp" />
+              <img alt="delivery" src="/images/deli.webp" />
             </a>
           </div>
           <ul className="signUpList">
@@ -64,53 +21,30 @@ export default class Nav extends Component {
                 회원가입
               </a>
             </li>
-            <li className="signUpList2">
+            <li className="signUpListTwo">
               <div></div>
             </li>
-            <li className="signUpList1">
+            <li className="signUpListOne">
               <a href="#">로그인</a>
             </li>
-            <li className="signUpList2">
+            <li className="signUpListTwo">
               <div></div>
             </li>
-            <li className="signUpList1">
+            <li className="signUpListOne">
               <a href="#">고객센터</a>
             </li>
           </ul>
         </div>
         <div className="secondLayout">
-          <img src="./images/logo2.png"></img>
+          <img alt="logo" src="./images/logo2.png"></img>
         </div>
         <div className="thirdLayout">
           <ul className="allCategorie">
-            <li onMouseLeave={this.downBoxEvent}>
-              <div className="cateLayout">
-                <a href="#">
-                  <i class="fas fa-bars"></i>
-                  <span>전체 카테고리</span>
-                </a>
-                <div
-                  className={
-                    this.state.isMouseHoverEvent
-                      ? "layoutMenu"
-                      : "layoutMenuNone"
-                  }
-                >
-                  <div className="menuDown">
-                    <ul className="menuDownList">
-                      {this.state.itemList.map(listitem => {
-                        return (
-                          <Navdropmenu
-                            list={listitem.list}
-                            key={listitem.id}
-                            item={listitem.item}
-                          />
-                        );
-                      })}
-                    </ul>
-                  </div>
-                </div>
-              </div>
+            <li onMouseEnter={this.downBoxEvent}>
+              <a href="#">
+                <i className="fas fa-bars" />
+                <span>전체 카테고리</span>
+              </a>
             </li>
             <li>
               <a href="#">신상품</a>
@@ -125,16 +59,16 @@ export default class Nav extends Component {
               <a href="#">특가/혜택</a>
             </li>
           </ul>
-          <div className="thirdLayout_icon">
+          <div className="thirdLayoutIcon">
             <div className="searchBar">
               <input placeholder="검색어를 입력해 주세요" />
               <i className="fas fa-search" />
             </div>
             <a href="#">
-              <i className="fas fa-map-marker-alt"></i>
+              <i className="fas fa-map-marker-alt" />
             </a>
             <a>
-              <i className="fas fa-shopping-cart"></i>
+              <i className="fas fa-shopping-cart" />
             </a>
           </div>
         </div>
