36f1378c2bafc1bbf24c8d617fa24868a55cd9ca
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-02 16:21:18 +0900

    Fix : 슬라이드창 구현중

---
+++ src/pages/Main/ProductListTitle.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ProductListTitle extends Component {
+  render() {
+    return (
+      <div className="productList">
+        <div className="productStatus">
+          <h3>이 상품 어때요?</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductListTitle;
