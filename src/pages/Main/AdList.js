6c9ed5ca929cbe17aea53105dedd464596047757
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-02 09:58:34 +0900

    Add,Fix : 슬라이드창 구현중

--- src/pages/Main/AdList.js
+++ src/pages/Main/AdList.js
@@ -1,10 +1,80 @@
 import React, { Component } from "react";
 
 class AdList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adImg: 1,
+      buttonDisplay: "none",
+    };
+  }
+
+  imgChangeLeft = () => {
+    if (this.state.adImg > 3) {
+      this.setState({
+        adImg: 1,
+      });
+    } else {
+      this.setState({
+        adImg: this.state.adImg + 1,
+      });
+    }
+  };
+
+  imgChangeRight = () => {
+    this.setState({
+      adImg: 4,
+    });
+    if (this.state.adImg <= 1) {
+      this.setState({
+        adImg: 3,
+      });
+    } else {
+      this.setState({
+        adImg: this.state.adImg - 1,
+      });
+    }
+  };
+
+  buttonDisplayOn = () => {
+    this.setState({
+      buttonDisplay: "block",
+    });
+  };
+  buttonDisplayOff = () => {
+    this.setState({
+      buttonDisplay: "none",
+    });
+  };
+
+  componentDidMount() {
+    setInterval(this.imgChangeLeft, 5000);
+  }
+
   render() {
     return (
-      <div className="adList">
-        <img src="./images/ad.webp" />
+      <div
+        className="adList"
+        onPointerOver={this.buttonDisplayOn}
+        onPointerOut={this.buttonDisplayOff}
+      >
+        <div className="adBanner">
+          <botton
+            className="leftArrow"
+            onClick={this.imgChangeLeft}
+            style={{ display: this.state.buttonDisplay }}
+          >
+            <img src="/images/leftbutton.webp" />
+          </botton>
+          <img alt="ad" src={`./images/${this.state.adImg}.webp`} />
+          <botton
+            className="rightArrow"
+            onClick={this.imgChangeRight}
+            style={{ display: this.state.buttonDisplay }}
+          >
+            <img src="/images/rightbutton.webp" />
+          </botton>
+        </div>
       </div>
     );
   }
--- src/pages/Main/CenterAdd.js
+++ src/pages/Main/CenterAdd.js
@@ -3,10 +3,8 @@ import React, { Component } from "react";
 class CenterAdd extends Component {
   render() {
     return (
-      <div>
-        <div className="centerAdd">
-          <img src="./images/ad2.webp" />
-        </div>
+      <div className="centerAdd">
+        <img src="./images/ad2.webp" />
       </div>
     );
   }
