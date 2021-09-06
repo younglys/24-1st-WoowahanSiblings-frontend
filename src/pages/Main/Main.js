b01b82b99261814272a506a28727d587dcefddf2
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-09-06 17:39:36 +0900

    Fix : 메인 창 슬라이드 부분 수정 중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -173,54 +173,6 @@ export default class Main extends Component {
           );
         })}
         <CenterAdd />
-        <div className="footer">
-          <div className="footerInfo">
-            <div className="infoCustomerCenter">고객 행복 센터</div>
-            <div className="infoNumber">
-              <div>1644-1107</div>
-              <div className="operatingHours">
-                <span>365고객센터</span>
-                <span>오전 7시 ~ 오후 7시</span>
-              </div>
-            </div>
-            <div className="kakao">
-              <div className="kakaoBox">카카오톡 문의</div>
-              <div className="kakaoOperatingHours">
-                <span>365고객센터</span>
-                <span className="infoTextColor">오전 7시 ~ 오후 7시</span>
-              </div>
-            </div>
-            <div className="oneAndOne">
-              <div className="oneAndOneBox">1:1문의</div>
-              <div className="oneAndOneOperatingHours">
-                <span>24시간 접수 가능</span>
-                <span className="infoTextColor">
-                  고객센터 운영시간에 순차적으로 답변해드리겠습니다.
-                </span>
-              </div>
-            </div>
-            <div className="bulkOrder">
-              <div className="bulkOrderBox">대량주문 문의</div>
-              <div className="bulkOrderOperatingHours">
-                <span>24시간 접수 가능</span>
-                <span className="infoTextColor">
-                  비회원의 겨우 메일로 문의 바랍니다.
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="company">
-            <ul className="companyList">
-              <li>컬리소개</li>
-              <li>컬리소개영상</li>
-              <li>인재채용</li>
-              <li>이용약관</li>
-              <li>개인정보처리방침</li>
-              <li>이용안내</li>
-            </ul>
-          </div>
-        </div>
       </div>
     );
   }
